0ed1ddc28d4382a3bcb407ef7bac2fa5002676e2
hidu <duv123+github@gmail.com>
2021-01-19 10:57:15 +0800

    add hello

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/fsgo/mod4
+module fsgo/mod4
 
 go 1.14
 
 require (
 	github.com/fsgo/mod1 v1.0.2
 	github.com/fsgo/mod2 v1.0.0
-	github.com/fsgo/mod3 v1.0.1
+	github.com/fsgo/mod3 v1.0.2
 )
---
+++ internal/say.go
@@ -0,0 +1,15 @@
+/*
+ * Copyright(C) 2021 github.com/hidu  All Rights Reserved.
+ * Author: hidu (duv123+git@baidu.com)
+ * Date: 2021/1/19
+ */
+
+package internal
+
+import (
+	"fmt"
+)
+
+func Say(msg string) {
+	fmt.Println(msg)
+}
